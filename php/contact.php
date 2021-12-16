7c60df3851d503306a27ce38218c343dda449abd
mikedick <mikedick@umich.edu>
2021-12-15 22:45:17 -0500

    Wrote more CSS for projects to try and fix link errors.

--- php/contact.php
+++ php/contact.php
@@ -8,14 +8,15 @@ if(isset($_POST['submit']))
     $subject = $_POST['subject'];
     $mail_from = $_POST['mail'];
     $message = $_POST['message'];
+
     //Setting variable to my email.
     $my_email = "mikedick@umich.edu";
     //See who this is from.
     $headers = "From: ". $mail_from;
     //Custom message to my email when contact form is filled out.
-    $txt = "You have received an email from". $name.".\n\n".$message;
+    $txt = "You have received an email from ". $name. ".\n\n".$message;
     //Mail to, subject, the message, and applicable headers.
-    mail($my_email, $subject, $txt,$headers);
+    mail($my_email, $subject, $txt, $headers);
     header("Location: index.php?mailsend");
 }
 ?>
