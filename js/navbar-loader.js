f5bb54341721be4c7395db9ff21bd00d97ddbc78
Michael Dick <michaeldick000@gmail.com>
2024-01-04 16:20:35 -0500

    made css more central. Added js to control navbar

--- js/navbar-loader.js
+++ js/navbar-loader.js
@@ -1,23 +1,39 @@
-document.addEventListener('DOMContentLoaded', (event) => {
-    // fetch('../html/navbar.html')
-    let navbar_path;
-    if (window.location.pathname.endsWith('index.html')) 
-    {
-        navbar_path = "html/navbar.html"
-    }
-    else
-    {
-        navbar_path = "../html/navbar.html"
-    }
-    fetch(navbar_path)
-    .then(response => {
-        return response.text()
-    })
+document.addEventListener('DOMContentLoaded', () => {
+    let navbarPath;
+    let isInsideHtmlDirectory = window.location.pathname.includes('/html/');
+
+    // Determine the correct path to the navbar file
+    navbarPath = isInsideHtmlDirectory ? '../html/navbar.html' : 'html/navbar.html';
+
+    // Fetch and load the navbar
+    fetch(navbarPath)
+    .then(response => response.text())
     .then(data => {
         document.getElementById('navbar').innerHTML = data;
+
+        // Update the href attributes of anchor tags within the navbar
+        document.querySelectorAll('#navbar a').forEach(link => {
+            let href = link.getAttribute('href');
+
+            // Adjust href based on current directory
+            if (isInsideHtmlDirectory) 
+            {
+                // Remove 'html/' from href if in the html directory
+                if (href.includes('html/')) 
+                {
+                    link.setAttribute('href', href.replace('html/', ''));
+                }
+                if (href.match("index.html"))
+                {
+                    console.log(href);
+                    // link.setAttribute('href', href.replace('html/', '../'));
+                    link.setAttribute('href', '../index.html')
+                    console.log(href);
+                }
+            } 
+        });
     })
-    .catch( error => {
+    .catch(error => {
         console.error("Error loading the navbar: ", error);
-    })
-    console.log("hello from navbar-loader")
-})
+    });
+});
